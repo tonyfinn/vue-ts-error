2cd310a74e67e8c1893984108d208e24e97d7563
Tony Finn <git@tonyfinn.com>
2021-05-22 17:14:54 +0100

    Add sample cases

---
+++ works/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <p @click="clickHandler">Sample TS Error: 
+  {{foo.bar}}</p>
+</template>
+
+<script lang="ts">
+
+import { defineComponent } from 'vue';
+
+class Foo {
+  constructor(readonly bar: string, private readonly baz: string) {
+    console.log(bar);
+    console.log(baz);
+  }
+  log(): void {
+    console.log(`Bar: ${this.bar}, Baz: ${this.baz}`)
+  }
+}
+
+function usesFoo(foo: Foo) {
+  foo.log();
+}
+
+
+export default defineComponent({
+  name: 'App',
+  data() {
+    const foo = new Foo('bar', 'baz');
+    usesFoo(foo);
+    return {
+      foo
+    }
+  },
+  methods: {
+    clickHandler() {
+      usesFoo(this.foo);
+    }
+  }
+})
+</script>
